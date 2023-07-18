9f413c6463164f420f6bc3d61a2d8b9ea6178434
OmoriRiku <121606498+OmoriRiku@users.noreply.github.com>
2023-07-18 22:34:47 +0900

    Java-silver

---
+++ kuro/1/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int total = tax(2, 2);
+    System.out.println(total);
+  }
+
+  public static int tax(int a, int b) {
+    return a + b;
+  }
+}
